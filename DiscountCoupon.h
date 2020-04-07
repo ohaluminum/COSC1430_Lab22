24d9780ad0176e4af7d308646089d92bd7d8cd48
Lejing Huang <ohaluminum@users.noreply.github.com>
2020-04-07 12:32:26 -0500

    Build Coupon and DiscountCoupon class.

--- DiscountCoupon.h
+++ DiscountCoupon.h
@@ -5,18 +5,40 @@
 #ifndef DISCOUNTCOUPON_H
 #define DISCOUNTCOUPON_H
 
-class DiscountCoupon : public Coupon{
+class DiscountCoupon : public Coupon {
 public:
-      DiscountCoupon(float off = 0) : Coupon(off)
-      {
-          if (off > 1 || off < 0)
-              setCoupon(0.);
-      }
-      
-      //FIX ME: please provide the overriding applyCoupon() function
-      
-      //FIX ME: please define the destructor for DiscountCoupon class
+
+    /*
+     *A constructor with one parameter(with default value).
+     *This parameter will be used to initialize the private member inherited from the Coupon class
+     */
+    DiscountCoupon(float off = 0) 
+        : Coupon(off)
+    {
+        if (off > 1 || off < 0)
+        setCoupon(0.0);
+    }
+
+    //A destructor that prints out the message Discount coupon is used!
+    ~DiscountCoupon()
+    {
+        cout << "Discount coupon is used!" << endl;
+    }
+
+    /*
+     *The overriding function float applyCoupon(float orgPrice) that re-implements the strategy of applying coupon.
+     *In particular, the new price after applying coupon will be (1 - amount) * orgPrice.
+     */
+
+    float applyCoupon(float orgPrice)
+    {
+        orgPrice = orgPrice * (1 - getCoupon());
+        return orgPrice;
+    }
       
 };
 
 #endif //DISCOUNTCOUPON_H
+
+
+
